79078ecb132291bf34674cac9281f50dfada4612
Adithya Polisetty <polisetty1489adithya@gmail.com>
2026-03-07 13:41:50 +0530

    Added flowchart and ER diagram documentation

--- task3-nestjs/src/auth/google.strategy.ts
+++ task3-nestjs/src/auth/google.strategy.ts
@@ -6,11 +6,11 @@ import { Strategy, Profile } from 'passport-google-oauth20';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: '1067875526169-oivgh0fptk46nsptlafqkl8976rk1mt.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
-      scope: ['email', 'profile'],
-    });
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
+  scope: ['email', 'profile'],
+});
   }
 
   async validate(
